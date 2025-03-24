36cc939954342ecea39078ef4b93c2dfcc73f37f
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2025-03-24 09:26:52 +0700

    Site layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <template>
-  <div></div>
-  >
+  <div id="app">
+    <SideBar />
+    <div class="main">
+      <div>hello</div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SideBar from "./components/Navigation/SideBar.vue";
+
 export default {
   name: "App",
+  components: {
+    SideBar,
+  },
 };
 </script>
 
-<style></style>
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: row;
+  font-family: "Inter", sans-serif;
+  height: 100dvh;
+  width: 100%;
+}
+
+.main {
+  flex-grow: 1;
+  background-color: #f8f7fa;
+  padding: 10px;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/Navigation/SideBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="sidebar">Sidebar Area</div>
+</template>
+
+<style scoped>
+.sidebar {
+  width: 250px;
+  border-right: 1px solid #bbb;
+}
+</style>
